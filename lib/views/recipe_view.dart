0696bae7649087fc179adb8ca792b1498758af96
toykam <mwork063@gmail.com>
2021-07-14 11:04:37 +0100

    still on initial commit

--- lib/views/recipe_view.dart
+++ lib/views/recipe_view.dart
@@ -36,58 +36,60 @@ class _RecipeViewState extends State<RecipeView> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: Column(
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.only(top: Platform.isIOS? 60: 30, right: 24,left: 24,bottom: 16),
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                      colors: [
-                        const Color(0xff213A50),
-                        const Color(0xff071930)
-                      ],
-                      begin: FractionalOffset.topRight,
-                      end: FractionalOffset.bottomLeft)),
-              child:  Row(
-                mainAxisAlignment: kIsWeb
-                    ? MainAxisAlignment.start
-                    : MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    "AppGuy",
-                    style: TextStyle(
-                        fontSize: 18,
-                        color: Colors.white,
-                        fontFamily: 'Overpass'),
-                  ),
-                  Text(
-                    "Recipes",
-                    style: TextStyle(
-                        fontSize: 18,
-                        color: Colors.blue,
-                        fontFamily: 'Overpass'),
-                  )
-                ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.only(top: Platform.isIOS? 60: 30, right: 24,left: 24,bottom: 16),
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        colors: [
+                          const Color(0xff213A50),
+                          const Color(0xff071930)
+                        ],
+                        begin: FractionalOffset.topRight,
+                        end: FractionalOffset.bottomLeft)),
+                child:  Row(
+                  mainAxisAlignment: kIsWeb
+                      ? MainAxisAlignment.start
+                      : MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      "Recipe ",
+                      style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.white,
+                          fontFamily: 'Overpass'),
+                    ),
+                    Text(
+                      "App",
+                      style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.blue,
+                          fontFamily: 'Overpass'),
+                    )
+                  ],
+                ),
               ),
-            ),
-            Container(
-              height: MediaQuery.of(context).size.height - (Platform.isIOS ? 104 : 30),
-              width: MediaQuery.of(context).size.width,
-              child: WebView(
-                onPageFinished: (val){
-                  print(val);
-                },
-                javascriptMode: JavascriptMode.unrestricted,
-                initialUrl: finalUrl,
-                onWebViewCreated: (WebViewController webViewController){
-                  setState(() {
-                    _controller.complete(webViewController);
-                  });
-                },
+              Container(
+                height: MediaQuery.of(context).size.height - (Platform.isIOS ? 104 : 30),
+                width: MediaQuery.of(context).size.width,
+                child: WebView(
+                  onPageFinished: (val){
+                    print(val);
+                  },
+                  javascriptMode: JavascriptMode.unrestricted,
+                  initialUrl: finalUrl,
+                  onWebViewCreated: (WebViewController webViewController){
+                    setState(() {
+                      _controller.complete(webViewController);
+                    });
+                  },
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       )
     );
